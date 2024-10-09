5e49660a05ccb377c038b70848e5eaf8ea535109
Nurdos_Zhanybekov <nurdos.zhanybekov@gmail.com>
2024-10-09 09:56:55 +0600

    add necessary files

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,33 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
 
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
-        System.out.println("Hello world!");
+        System.out.println("----------------------------");
+        System.out.println("Helping the Martians");
+        System.out.println("----------------------------");
+
+        int location_first_cargo = random.nextInt() % 7;
+        int location_second_cargo = random.nextInt() % 7;
+        int location_third_cargo = random.nextInt() % 7;
+
+        int distance_ship_city = 7;
+        int ship_location = 0;
+        int amount_of_boxes = 3;
+
+        int[] locations_of_the_cargos = new int[amount_of_boxes];
+
+        for(int i = 0; i < amount_of_boxes; i++){
+            locations_of_the_cargos[i] = scanner.nextInt();
+            if(locations_of_the_cargos[i] > distance_ship_city || locations_of_the_cargos[i] < ship_location){
+                System.out.println("Invalid Input");
+                System.exit(1);
+            }
+        }
+
+
     }
 }
